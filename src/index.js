d443d29fe27465d469f28e5ec1cf6670fa05a66f
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-09-06 18:20:52 +0600

    feat: add new code to solution

--- src/index.js
+++ src/index.js
@@ -1,37 +1,117 @@
 module.exports = function solveSudoku(matrix) {
-  var nonPossibilities = {},impossibleNumbers, emptySpaces = 81; 
-  while (emptySpaces >0) {
-    for (var vert = 0; vert < matrix.length; vert ++) { 
-      for (var horz = 0; horz < matrix.length; horz++) { 
-        if (matrix[vert] [horz] === 0) { 
-          nonPossibilities = {};
-          for (var i = 0; i < 9; i++) { 
-            if (matrix[vert] [i] > 0) {
-              nonPossibilities [matrix [vert] [i] ] = true 
-            } 
-            if (matrix[i][horz] > 0) { nonPossibilities[matrix[i][horz]] = true 
-            }
-          }
-          for (var vertBox = Math.floor(vert / 3) * 3; vertBox < Math.floor(vert/3)* 3 + 3; vertBox++){
-            for (var horzBox = Math.floor(horz/3) * 3; horzBox < Math.floor(horz/3)* 3 + 3; horzBox++){
-              if (matrix[vertBox][horzBox]){
-                nonPossibilities [matrix[vertBox] [horzBox]] = true;
+  let newArr = [];
+  matrix.forEach(element => {
+    newArr.push(...element)
+  });
+
+  const objSudo = {
+
+  }
+  let allDone = [];
+  let boxIndex = [
+    [0,  1,  2,   9, 10, 11 ,18 ,19 ,20],
+    [3,  4,  5,  12, 13, 14, 21, 22, 23],
+    [6,  7,  8,  15, 16, 17, 24, 25, 26],
+    [27, 28, 29, 36, 37, 38, 45, 46, 47],
+    [30, 31, 32, 39, 40, 41, 48, 49, 50],
+    [33, 34, 35, 42, 43, 44, 51, 52, 53],
+    [54, 55, 56, 63, 64, 65, 72, 73, 74],
+    [57, 58, 59, 66, 67, 68, 75, 76, 77],
+    [60, 61, 62, 69, 70, 71, 78, 79, 80]
+  ]
+
+  
+  function isResolve (value, index) {
+    function rowChek (value, index) {
+      let start = 0;
+      let end = 8;
+      if (index > 8) {
+        let k = Math.floor(index/9);
+        start = k * 9;
+        end = start + 8;
+      }
+      for (let i = start; i <= end; i++) {
+        if (value === newArr[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    function colChek (value, index) {
+      let start = index;
+      if (index > 8) {
+        start = index - Math.floor(index/9) * 9;
+      }
+      for (let i = start; i < newArr.length; i += 9) {
+        if (value === newArr[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+  
+    function minBlokCheck (value, index) {
+      for (let i = 0; i < boxIndex.length; i++) {
+        for (let j = 0; j < boxIndex[i].length; j++) {
+          if (boxIndex[i][j] == index){
+            for (let k = 0; k < boxIndex[i].length; k++) {
+              if (newArr[boxIndex[i][k]] === value) {
+                return false;
               }
             }
+            return true;
           }
-          impossibleNumbers = Object.keys(nonPossibilities); 
-          if (impossibleNumbers.length = 8) {
-            for (var i = 1; i < 10; i++) {
-              if (impossibleNumbers.indexOf(i.toString()) < 0) { 
-                matrix[vert][horz] = i;
-              }
-            }
+        }
+      }
+    }
+    if (rowChek(value, index) === true && (colChek(value, index) === true && minBlokCheck(value, index) === true)) {
+      return true
+    }
+    return false
+  }
+  
+
+  for (let j = 0; j < newArr.length; j++) {
+    if (newArr[j] === 0) {
+      for (let num = 1; num <= 9; num++) {
+        if (isResolve(num, j) === true) {
+          if (objSudo[`${j}`] == undefined){
+            objSudo[`${j}`] = [num]
           } else {
-            emptySpaces++;
+            objSudo[j].push(num)
           }
         }
       }
     }
   }
-  return matrix;
+  const keys = Object.keys(objSudo);
+  for (let i = 0; i < keys.length; i++) {
+    for (let j = 0; j < objSudo[keys[i]].length; j++) {
+      if (newArr[keys[i]] != 0) {
+        j = objSudo[keys[i]].findIndex(element => element == newArr[keys[i]]) + 1;
+        newArr[keys[i]] = 0;
+      }
+      while (j == objSudo[keys[i]].length){
+        i = i - 1;
+        j = objSudo[keys[i]].findIndex(element => element == newArr[keys[i]]) + 1
+        newArr[keys[i]] = 0;
+        if (i == 0 && j == objSudo[keys[i]].length) return console.log (`imposible!`)
+      }
+
+      if (isResolve(objSudo[keys[i]][j], Number(keys[i])) === true) {
+        newArr[keys[i]] = objSudo[keys[i]][j]
+        j = 0;
+        break;
+      }
+    }
+    if (newArr[keys[i]] === 0) {
+      i = i - 2
+    }
+  }
+  for (let i = 0; i < 9; i++) {
+    allDone.push(newArr.slice(i * 9, i*9 +9))
+  }
+  return allDone
 }
