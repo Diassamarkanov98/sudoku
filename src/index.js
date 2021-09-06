b4cedf05dcc46dab39eccab579fa4d49a9a57e50
Dias Samarkanov <77679797+Diassamarkanov98@users.noreply.github.com>
2021-09-06 18:18:28 +0600

    feat: add circle else

--- src/index.js
+++ src/index.js
@@ -26,6 +26,8 @@ module.exports = function solveSudoku(matrix) {
                 matrix[vert][horz] = i;
               }
             }
+          } else {
+            emptySpaces++;
           }
         }
       }
